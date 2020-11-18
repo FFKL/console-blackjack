9ddd446dcff8fde263a978fa7292c530d7f92abc
dmitrii <ffklffkl@gmail.com>
2020-11-18 23:07:17 +0200

    add src dir

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-g++ -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -Werror -std=c++17 -o blackjack main.cpp game.cpp session.cpp console.cpp cards.cpp 
+g++ \
+  -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -Werror \
+  -std=c++17 -o blackjack \
+  src/main.cpp src/game.cpp src/session.cpp src/console.cpp src/cards.cpp
