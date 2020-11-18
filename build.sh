da1ce11b9c515ba351ad7584316ab83e608a5bf2
dmitrii <ffklffkl@gmail.com>
2020-11-19 00:45:13 +0200

    init cmake

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-g++ \
-  -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -Werror \
-  -std=c++17 -o blackjack \
-  src/main.cpp src/game.cpp src/session.cpp src/console.cpp src/cards.cpp
