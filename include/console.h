08b88deaabccd00d9926b2a44f91ef7bac3f1fe2
dmitrii <ffklffkl@gmail.com>
2020-11-21 01:06:56 +0200

    add command line args parsing

--- include/console.h
+++ include/console.h
@@ -5,6 +5,11 @@
 
 namespace Console
 {
+  enum class Options
+  {
+    SuppressColor = 1 << 0,
+    UnicodeCards = 1 << 1,
+  };
   void preventInvalidInput();
   std::string green(std::string);
   std::string red(std::string);
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include "Session.h"
+#include "console.h"
 
-int main()
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <assert.h>
+
+unsigned char getConsoleOptions(int argc, char *argv[])
 {
+  unsigned char options{0};
+  for (int i{1}; i < argc; ++i)
+  {
+    auto arg{argv[i]};
+    if (!strcmp(arg, "--suppress-color"))
+    {
+      options |= static_cast<char>(Console::Options::SuppressColor);
+      continue;
+    }
+
+    if (!strcmp(arg, "--unicode-cards"))
+    {
+      options |= static_cast<char>(Console::Options::UnicodeCards);
+      continue;
+    }
+
+    std::cerr << "Invalid argument - " << arg << '\n';
+    exit(1);
+  }
+
+  return options;
+}
+
+int main(int argc, char *argv[])
+{
+  auto options{getConsoleOptions(argc, argv)};
+  std::cout << static_cast<int>(options) << '\n';
   Session session;
   session.play();
   return 0;
